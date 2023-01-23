1304696dc256dc6e82f1f89ab2328d97f2645918
Jason A. Arter <jason.arter@gmail.com>
2023-01-23 11:10:19 -0800

    HTML and scripting complete.

--- index.js
+++ index.js
@@ -1,9 +1,44 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
+
 const writeHtml = function (user, loc, blurb, linked, git) {
-    console.log(user, loc, blurb, linked, git);
+    htmlString = `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="./assets/css/style.css">
+        <title>Portfolio for ${user}</title>
+    </head>
+    <body>
+        <header>
+            <div>
+                <h1 id="name">${user}</h1>
+            </div>
+            <div>
+                <h2 id="location">${loc}</h2>
+            </div>
+        </header>
+        <main>
+            <div>${blurb}</div>
+            <div><a href="https://www.linkedin.com/in/${linked}">${linked}</a></div>
+            <div><a href="https://github.com/${git}">${git}</a></div>
+        </main>
+    </body>
+    </html>
+    `
+    fs.writeFile('bio.html', htmlString, function (err) {
+        if (err) {
+            console.log("Error!");
+        } else {
+            console.log("File written.");
+        }
+    }
+    );
 };
+
 
 const init = function () {
     inquirer.prompt([
@@ -33,11 +68,11 @@ const init = function () {
             name: 'gitHub',
         },
     ]).then((response) =>
-    writeHtml(response.username, 
-        response.location, 
-        response.bio, 
-        response.linkedIn, 
-        response.gitHub)
+        writeHtml(response.username,
+            response.location,
+            response.bio,
+            response.linkedIn,
+            response.gitHub)
     );
 }
 
